ab94a20bf3a7d688f248125c5bbc47c9aac027a1
karaler <karaler@bytelang.com>
2020-12-11 22:34:51 +0800

    fix: 修复参数无效的问题

--- KPVideoShowImagePlugin.cpp
+++ KPVideoShowImagePlugin.cpp
@@ -8,7 +8,7 @@
 
 #include "KPVideoShowImagePlugin.h"
 
-KPVideoShowImagePlugin::KPVideoShowImagePlugin(const std::string &identify_name, const std::string &filter_name, const KPFilterType &filter_type, PluginParamsObject plugin_params_object) : KPPluginAdapter(identify_name, filter_name, filter_type, std::move(plugin_params_object)) {
+KPVideoShowImagePlugin::KPVideoShowImagePlugin(const std::string &identify_name, const std::string &filter_name, const KPFilterType &filter_type, PluginParamsObject plugin_params_object) : KPPluginAdapter(identify_name, filter_name, filter_type,plugin_params_object) {
     // 赋值described
     std::stringstream filter_desc_stream;
 
